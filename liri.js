6085c61b27cb0bafd2741d1d179042044d7a04a9
chrisyshorshort <chritinasarahwu@gmail.com>
2018-03-28 18:57:56 -0700

    added log txt and fixed spotify

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 // To use the .env file //
 require("dotenv").config();
 var fs = require('fs');
-var key = require("./keys.js");
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var request = require('request');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
@@ -25,7 +28,10 @@ function getTweets() {
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         for (var i= 0; i <tweets.length; i++) {
             if (error) {console.log(error)} else {
-            console.log("Date: " + tweets[i].created_at + "\n\tTweet: " + tweets[i].text)
+            console.log("Date: " + tweets[i].created_at + "\nTweet: " + tweets[i].text + "\n")
+            fs.appendFile('log.txt', ("Date: " + tweets[i].created_at + "\nTweet: " + tweets[i].text + "\n"), function(error){
+                if (error) {console.log(error)}
+            })
             }
         }
     })
@@ -38,10 +44,17 @@ function getSpotify() {
         console.log("No song selected, going to default song.")
         nameOfSong = "The Sign Ace of Base"
     }
-    var spotifyResponse = data.tracks.items;
-    for (var i = 0; i <spotifyResponse.length; i++) {
-        console.log("Song: " + spotifyResponse[0].name + "\nArtist: " + spotifyResponse[0].artist[0].name + "\nAlbum: " + spotifyResponse[0].album.name + "\nLink: " + spotifyResponse[0].album.external_urls.spotify)
-    }
+
+    spotify.search({type: 'track', query: nameOfSong}, function (error, data){
+        if (error) { console.log(error); 
+            return;
+        }
+        var spotifyResponse = data.tracks.items;
+            console.log("Song: " + spotifyResponse[0].name + "\nArtist: " + spotifyResponse[0].album.artists[0].name + "\nAlbum: " + spotifyResponse[0].album.name + "\nLink: " + spotifyResponse[0].external_urls.spotify)
+        fs.appendFile('log.txt', ("Song: " + spotifyResponse[0].name + "\nArtist: " + spotifyResponse[0].album.artists[0].name + "\nAlbum: " + spotifyResponse[0].album.name + "\nLink: " + spotifyResponse[0].external_urls.spotify), function(error){
+            if (error) {console.log(error)}
+        })
+    })
 }
 
 // Function for OMDB to get movie information //
@@ -54,6 +67,9 @@ function getMovie() {
     request('https://www.omdbapi.com?apikey=trilogy&t=' + movie + '&plot=full', function(err, response, body) {
         if (err) {console.log(err)} else{
             console.log("Title: " + JSON.parse(body)['Title'] + "\nYear: " + JSON.parse(body)['Year'] + "\nCountry: " + JSON.parse(body)['Country'] + "\nLanguage: " + JSON.parse(body)['Language'] + "IMBD Rating: " + JSON.parse(body)['imbdRating'] + "\nRotten Tomatoes Rating: " + JSON.parse(body)['Ratings']['Value'] + "\nActors: " + JSON.parse(body)['Actors'] + "\nPlot: " + JSON.parse(body)['Plot'])
+            fs.appendFile('log.txt', ("Title: " + JSON.parse(body)['Title'] + "\nYear: " + JSON.parse(body)['Year'] + "\nCountry: " + JSON.parse(body)['Country'] + "\nLanguage: " + JSON.parse(body)['Language'] + "IMBD Rating: " + JSON.parse(body)['imbdRating'] + "\nRotten Tomatoes Rating: " + JSON.parse(body)['Ratings']['Value'] + "\nActors: " + JSON.parse(body)['Actors'] + "\nPlot: " + JSON.parse(body)['Plot']), function(error){
+                if (error) {console.log(error)}
+            })
         }
     })
 }
